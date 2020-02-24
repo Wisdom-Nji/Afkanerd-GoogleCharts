ee65fcd5dadfe67db9599a0fca2c336708159434
Sherlock Wisdom <wisdomnji@gmail.com>
2020-02-24 13:05:32 +0100

    created Graph class

---
+++ graphs.js
@@ -0,0 +1,19 @@
+
+class Graphs {
+	this.type = ""
+	constructor( DOMLocation ) {}
+
+	addSlicer( cl_slicers ) // cl_% = class
+
+	setTitle( title )
+
+	setWidth( width )
+
+	setHeight( height )
+
+	setLengendPosition( position )
+
+	setOption( option )
+
+	render()
+}
