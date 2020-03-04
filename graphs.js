7376af34ebbd1b25d9682d58b865ef6c2b7828d5
Sherlock Wisdom <wisdomnji@gmail.com>
2020-03-04 08:13:11 +0100

    did some final work, then power went off

--- graphs.js
+++ graphs.js
@@ -52,36 +52,57 @@ class Graphs {
 		this.option = option;
 	}
 
+	bindData( data ) {
+		this.data = data;
+	}
+
+	getData( independentVariable, values ) {
+		return new Promise( (resolve, reject)=> {
+			let v_data = []
+			for(let i in this.data ) {
+				if(values.findIndex( variables => this.data[i].independentVariable == variables ) == -1 ) resolve( [] );
+				v_data.push( this.data[i] );
+			}
+			resolve(v_data);
+		});
+
+	}
+
+	render( data ) {
+		var chart;
+		this.graphData.addRows( data );
+		for(let i in this.columnCollection)  {
+			this.graphData.addColumn( this.columnCollection[i][0], this.columnCollection[i][1] );
+		}
+		//TODO: if type of data is date, it should be split and turned into date format: new Date(Y, M, D)
+		//TODO: Remember to minus -1 from months cus JS dates begin from 0 = January
+		switch( this.type ) {
+			case "bar":
+			chart = new this.google.visualization.BarChart( this.DOMElement )	
+			//Options can be NULL when passed
+			break;
+
+			case "pie":
+			chart = new this.google.visualization.PieChart( this.DOMElement )	
+			//Options can be NULL when passed
+			break;
+		}
+		chart.draw(this.graphData, this.options);
+	}
+
 	addSlicer( slicer ) {
-		slicer.DOMElement.onchange = ()=>{
+		slicer.DOMElement.onchange = async ()=>{
 			console.log("=> Graph should change with slicer");
 			if(typeof slicer.independentVariable == "undefined") {
 				console.error("=> colValue for slicer undefined");
 				return;
 			}
 
-			console.log("=> Adjusting graph for:", slicer.independentVariable );
-		}
-	}
+			console.log("=> Adjusting graph for:", slicer.independentVariable, slicer.dependentVariable);
 
-	render( data ) {
-		//TODO: This graph draws here
-		for(let i in this.columnCollection) 
-			this.graphData.addColumn( this.columnCollection[i][0], this.columnCollection[i][1] );
-		this.graphData.addRows( data );
-		var chart;
-		switch( this.type ) {
-			case "bar":
-				chart = new this.google.visualization.BarChart( this.DOMElement )	
-				//Options can be NULL when passed
-			break;
-
-			case "pie":
-				chart = new this.google.visualization.PieChart( this.DOMElement )	
-				//Options can be NULL when passed
-			break;
+			let data = await getData(slicer.independentVariable, slicer.values);
+			render( data );
 		}
-		chart.draw(this.graphData, this.options);
 	}
 
 }
