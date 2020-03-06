e77bc9107309a75348ec96404759144afb3053a1
Sherlock Wisdom <wisdomnji@gmail.com>
2020-03-06 23:41:44 +0100

    this is how it should work

--- graphs.js
+++ graphs.js
@@ -74,12 +74,16 @@ class Graphs {
 
 	}
 
+	set setLabel( label ) {
+		this.label = label;
+	}
+
 	render( data ) {
 		var chart;
 
 		let preparedData = (()=>{
 			//Some sanitation check of column
-			let v_data = [ [this.columns[0][1], this.columns[1][1], { role: 'annotation' }] ]
+			let v_data = typeof this.label != "undefined" ? [[this.columns[0][1], this.columns[1][1], { role: 'annotation' }] ] : [ [this.columns[0][1], this.columns[1][1]] ]
 			for(let i in data ) {
 				let split_date;
 				let oneD_axis = this.columns[0].findIndex(variable => 'date' == variable ) == -1 ? data[i][this.columns[0][1]] : new Date(data[i][this.columns[0][1]])
@@ -99,7 +103,10 @@ class Graphs {
 
 				// could have an option to choose the third annotation or not
 				// option to choose whether to use oneD_axis or twoD_axis
-				v_data.push([oneD_axis, twoD_axis, String(oneD_axis)]);
+				if( typeof this.label != "undefined" ) 
+					v_data.push([oneD_axis, twoD_axis, data[i][this.label]] );
+				else
+					v_data.push([oneD_axis, twoD_axis]);
 				//console.log("=>x_axis:",oneD_axis)
 				//console.log("=>y_axis:",twoD_axis)
 
